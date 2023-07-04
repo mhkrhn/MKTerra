798d64d65387fde691a46a8d97d61fc9ad9adf7b
mhkrhn <128690942+mhkrhn@users.noreply.github.com>
2023-07-04 16:29:17 +0200

    infra

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
 terraform {
+  required_version = ">=1.3.0"
+  required_providers {
+    azurerm = {
+      "source" = "hashicorp/azurerm"
+      version  = "3.43.0"
+    }
+  }
+
+ 
+  }
+terraform {
   cloud {
     organization = "mkterra"
 
